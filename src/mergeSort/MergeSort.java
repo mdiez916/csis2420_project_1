5102f80ed7f4e09607bd8f84bed02f3d00430e8e
maxh1231 <maxhumpherys@gmail.com>
2024-02-15 15:14:24 -0700

    implemented comparison counter to merge sort

--- src/mergeSort/MergeSort.java
+++ src/mergeSort/MergeSort.java
@@ -1,76 +1,76 @@
 package mergeSort;
 
 class MergeSort {
-	void merge(int arr[], int l, int m, int r) {
-		int n1 = m - l + 1;
-		int n2 = r - m;
+    private int comparisons = 0; // Class-level variable to count comparisons
 
-		int L[] = new int[n1];
-		int R[] = new int[n2];
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
-		for (int i = 0; i < n1; ++i)
-			L[i] = arr[l + i];
-		for (int j = 0; j < n2; ++j)
-			R[j] = arr[m + 1 + j];
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
-		int i = 0, j = 0;
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            comparisons++; // Increment the counter for each comparison
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
 
-		int k = l;
-		while (i < n1 && j < n2) {
-			if (L[i] <= R[j]) {
-				arr[k] = L[i];
-				i++;
-			} else {
-				arr[k] = R[j];
-				j++;
-			}
-			k++;
-		}
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
 
-		while (i < n1) {
-			arr[k] = L[i];
-			i++;
-			k++;
-		}
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
-		while (j < n2) {
-			arr[k] = R[j];
-			j++;
-			k++;
-		}
-	}
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
 
-	void sort(int arr[], int l, int r) {
-		if (l < r) {
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
 
-			int m = l + (r - l) / 2;
+            merge(arr, l, m, r);
+        }
+    }
 
-			sort(arr, l, m);
-			sort(arr, m + 1, r);
+    static void printArray(int arr[], int comparisons) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+        System.out.println("Total comparisons made: " + comparisons);
+    }
 
-			merge(arr, l, m, r);
-		}
-	}
+    public static void main(String args[]) {
+        MergeSort ob = new MergeSort();
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
 
-	static void printArray(int arr[]) {
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
+        System.out.println("Given array is");
+        printArray(arr, 0); // Initial call with 0 comparisons
 
-	public static void main(String args[]) {
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
+        ob.sort(arr, 0, arr.length - 1);
 
-		System.out.println("Given array is");
-		printArray(arr);
-
-		MergeSort ob = new MergeSort();
-		ob.sort(arr, 0, arr.length - 1);
-
-		System.out.println("\nSorted array is");
-		printArray(arr);
-	}
+        System.out.println("\nSorted array is");
+        printArray(arr, ob.comparisons); // Pass comparisons to the print method
+    }
 }
-
