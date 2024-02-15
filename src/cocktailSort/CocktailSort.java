fd117c8570caa2b6871a82845d0ddc14abc8491a
maxh1231 <maxhumpherys@gmail.com>
2024-02-15 15:08:58 -0700

    implemented comparison counter to cocktail

--- src/cocktailSort/CocktailSort.java
+++ src/cocktailSort/CocktailSort.java
@@ -1,55 +1,64 @@
 package cocktailSort;
 
 public class CocktailSort {
-	void cocktailSort(int a[]) {
-		boolean swapped = true;
-		int start = 0;
-		int end = a.length;
+    private int comparisons = 0; // Class-level variable to count comparisons
 
-		while (swapped == true) {
-			swapped = false;
+    void cocktailSort(int a[]) {
+        boolean swapped = true;
+        int start = 0;
+        int end = a.length;
 
-			for (int i = start; i < end - 1; ++i) {
-				if (a[i] > a[i + 1]) {
-					int temp = a[i];
-					a[i] = a[i + 1];
-					a[i + 1] = temp;
-					swapped = true;
-				}
-			}
+        // Reset the comparisons counter each time cocktailSort is called
+        comparisons = 0;
 
-			if (swapped == false)
-				break;
+        while (swapped) {
+            swapped = false;
 
-			swapped = false;
+            for (int i = start; i < end - 1; ++i) {
+                comparisons++; // Increment the counter for each comparison
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
 
-			end = end - 1;
+            if (!swapped)
+                break;
 
-			for (int i = end - 1; i >= start; i--) {
-				if (a[i] > a[i + 1]) {
-					int temp = a[i];
-					a[i] = a[i + 1];
-					a[i + 1] = temp;
-					swapped = true;
-				}
-			}
+            swapped = false;
 
-			start = start + 1;
-		}
-	}
+            end = end - 1;
 
-	void printArray(int a[]) {
-		int n = a.length;
-		for (int i = 0; i < n; i++)
-			System.out.print(a[i] + " ");
-		System.out.println();
-	}
+            for (int i = end - 1; i >= start; i--) {
+                comparisons++; // Increment the counter for each comparison
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    swapped = true;
+                }
+            }
 
-	public static void main(String[] args) {
-		CocktailSort ob = new CocktailSort();
-		int a[] = { 5, 1, 4, 2, 8, 0, 2 };
-		ob.cocktailSort(a);
-		System.out.println("Sorted array");
-		ob.printArray(a);
-	}
+            start = start + 1;
+        }
+    }
+
+    void printArray(int a[]) {
+        int n = a.length;
+        for (int i = 0; i < n; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
+        // Print the total number of comparisons after the array
+        System.out.println("Total comparisons made: " + comparisons);
+    }
+
+    public static void main(String[] args) {
+        CocktailSort ob = new CocktailSort();
+        int a[] = { 5, 1, 4, 2, 8, 0, 2 };
+        ob.cocktailSort(a);
+        System.out.println("Sorted array:");
+        ob.printArray(a);
+    }
 }
