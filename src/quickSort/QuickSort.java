621231caaa38f27fe44cac781823154187fc72a4
maxh1231 <maxhumpherys@gmail.com>
2024-02-15 16:09:10 -0700

    implemented comparison counter for quick sort

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -3,52 +3,57 @@ package quickSort;
 import java.io.*;
 
 class QuickSort {
+    static int comparisons = 0; // Class-level variable to count comparisons
 
-	static void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-	static int partition(int[] arr, int low, int high) {
-		int pivot = arr[high];
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
 
-		int i = (low - 1);
+        for (int j = low; j <= high - 1; j++) {
+            // Increment comparison counter for each comparison made
+            comparisons++;
 
-		for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return (i + 1);
+    }
 
-			if (arr[j] < pivot) {
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
 
-				i++;
-				swap(arr, i, j);
-			}
-		}
-		swap(arr, i + 1, high);
-		return (i + 1);
-	}
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
 
-	static void quickSort(int[] arr, int low, int high) {
-		if (low < high) {
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
-			int pi = partition(arr, low, high);
+    public static void main(String[] args) {
+        // Reset comparison counter at the start of the sorting process
+        comparisons = 0;
+        
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int N = arr.length;
 
-			quickSort(arr, low, pi - 1);
-			quickSort(arr, pi + 1, high);
-		}
-	}
-
-	public static void printArr(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-	}
-
-	public static void main(String[] args) {
-		int[] arr = { 10, 7, 8, 9, 1, 5 };
-		int N = arr.length;
-
-		quickSort(arr, 0, N - 1);
-		System.out.println("Sorted array:");
-		printArr(arr);
-	}
+        quickSort(arr, 0, N - 1);
+        System.out.println("Sorted array:");
+        printArr(arr);
+        System.out.println("Total comparisons made: " + comparisons);
+    }
 }
